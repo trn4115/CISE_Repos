a2208b048a39a98e54df29c1a8a674760af6e748
Lucas Hashemi <trn4115@aut.ac.nz>
2024-08-14 22:11:15 +1200

    feat(app): create a simple interactive app with increment button
    - Added a basic interactive component that displays a counter with an increment button.
    - This component allows the user to increase the counter by clicking the button.

--- worksheet2/my-react-app/src/App.tsx
+++ worksheet2/my-react-app/src/App.tsx
@@ -1,14 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
-    </div>
-  );
+import React, { Component } from "react";
+interface State {
+  count: number;
 }
-
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  makeIncrementer = (amount: number) => () =>
+    this.setState((prevState: State) => ({
+      count: prevState.count + amount,
+    }));
+  increment = this.makeIncrementer(1);
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
