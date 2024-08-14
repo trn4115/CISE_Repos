abe5cb20da031d70416a74e5829af0aa3f78bb04
Lucas Hashemi <trn4115@aut.ac.nz>
2024-08-14 21:12:11 +1200

    chore(env): set up environment variables with .env file
    - Added a `.env` file to manage environment variables, such as the server port. Updated `main.ts` to use the PORT variable.

--- worksheet2/my-nest-app/src/main.ts
+++ worksheet2/my-nest-app/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  console.log(`PORT from .env: ${process.env.PORT}`);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT);
+  console.log(`Server is running on http://localhost:${PORT}`);
 }
 bootstrap();
