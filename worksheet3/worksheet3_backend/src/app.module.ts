50d8d6dd7370ea91ccc6455aa6591274d08003b7
Lucas Hashemi <trn4115@aut.ac.nz>
2024-08-15 12:38:26 +1200

    chore(database): set up MongoDB cluster and connection
    - Set up a new MongoDB cluster using MongoDB Atlas.
    - Configured the connection string for the Nest.js application.
    - Integrated MongoDB into the project using the Mongoose module, enabling seamless interaction with the database.

--- worksheet3/worksheet3_backend/src/app.module.ts
+++ worksheet3/worksheet3_backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Loads environment variables
+    MongooseModule.forRoot(process.env.DB_URI),
+  ], // Uses the DB_URI from .env file
   controllers: [AppController],
   providers: [AppService],
 })
