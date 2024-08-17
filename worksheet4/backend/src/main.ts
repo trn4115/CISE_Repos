d80dae229e2ae65b039989039ad5707b927a130b
Lucas Hashemi <trn4115@aut.ac.nz>
2024-08-17 17:45:10 +1200

    feat: enable CORS
    - Added CORS support with origin set to true and credentials enabled.

--- worksheet4/backend/src/main.ts
+++ worksheet4/backend/src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   dotenv.config();
   console.log(`PORT from .env: ${process.env.PORT}`);
   const app = await NestFactory.create(AppModule);
+  // enable cors
+  app.enableCors({ origin: true, credentials: true });
   const PORT = process.env.PORT || 8082;
   await app.listen(PORT, () =>
     console.log(`Server running on http://localhost:${PORT}`),
